831393deba66ffc5ef84b899ac2f21372e01add9
ved <ved.lucifer@gmail.com>
2021-11-04 00:59:01 +0530

    Added lazy load on home page

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -4,45 +4,49 @@ import Footer from '../Footer';
 import Header from '../Header';
 import './style.css';
 import { images } from '../../ImageList'
+import LazyLoad from "react-lazyload";
+
 
 const LandingPage = () => {
 
-    
 
-    function FadeInSection(props){
-        const [isVisible, setVisible] = React.useState(false);
-        const domRef = React.useRef();
-        React.useEffect(() => {
-          const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => setVisible(entry.isIntersecting));
-          });
-          observer.observe(domRef.current);
-        }, []);
-        return (
-          <div
-            className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-            ref={domRef}
-          >
-            {props.children}
-          </div>
-        );
-    }
 
+  function FadeInSection(props) {
+    const [isVisible, setVisible] = React.useState(false);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+    }, []);
     return (
-        <div>
-            <Header />
-            <LandingPageContainer>
-                {
-                    images.map((url,index)=>(
-                        <FadeInSection>
-                            <Pictures src={url} key={index} data-id={index}/>
-                        </FadeInSection>
-                    ))
-                }
-            </LandingPageContainer>
-            <Footer />
-        </div>
-    )
+      <div
+        className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
+        {props.children}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Header />
+      <LandingPageContainer>
+        {
+          images.map((url, index) => (
+            <FadeInSection>
+              <LazyLoad once>
+                <Pictures src={url} key={index} data-id={index} />
+              </LazyLoad>
+            </FadeInSection>
+          ))
+        }
+      </LandingPageContainer>
+      <Footer />
+    </div>
+  )
 }
 
 export default LandingPage;
